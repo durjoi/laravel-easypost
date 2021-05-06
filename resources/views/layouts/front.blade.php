734cf7387de6278c63c54c06dd76cf892f562751
glenn13 <f.glenn.abalos>
2021-05-07 00:26:07 +0400

    fix bug in registration

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -40,8 +40,8 @@
 
     <!-- Favicons -->
     <link rel="apple-touch-icon" href="https://getbootstrap.com/docs/5.0/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-    <link rel="icon" href="https://getbootstrap.com/docs/5.0/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-    <link rel="icon" href="https://getbootstrap.com/docs/5.0/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+    <link rel="icon" href="{{ url('library/images/favicon.ico') }}" sizes="32x32" type="image/png">
+    <link rel="icon" href="{{ url('library/images/favicon.ico') }}" sizes="16x16" type="image/png">
     <link rel="manifest" href="https://getbootstrap.com/docs/5.0/assets/img/favicons/manifest.json">
     <link rel="mask-icon" href="https://getbootstrap.com/docs/5.0/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
 
